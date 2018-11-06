f01e7d007071cc85d20584cea12f2df82c1fd93e
nrifath2009 <nrifath2009@gmail.com>
2018-11-06 02:42:30 -0600

    Add project files.

---
+++ EmailSendExampleApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailSendExampleApp.Startup))]
+namespace EmailSendExampleApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
