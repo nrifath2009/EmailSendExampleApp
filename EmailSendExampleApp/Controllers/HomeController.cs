801aed7951ae50b7d186450641919a48c98a1ff1
nrifath2009 <nrifath2009@gmail.com>
2018-11-06 03:17:19 -0600

    Implement Email Sending

--- EmailSendExampleApp/Controllers/HomeController.cs
+++ EmailSendExampleApp/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using EmailSendExampleApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +29,55 @@ namespace EmailSendExampleApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SendEmail(EmailSendModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                    var message = new MailMessage();
+                    message.To.Add(new MailAddress("user@outlook.com"));  // replace with valid value 
+                    message.From = new MailAddress(model.SenderEmail);  // replace with valid value
+                    message.Subject = "Test Email From ASP.NET MVC";
+                    message.Body = string.Format(body, model.SenderName, model.SenderEmail, model.Message);
+                    message.IsBodyHtml = true;
+
+                    using (var smtp = new SmtpClient())
+                    {
+                        //var credential = new NetworkCredential
+                        //{
+                        //    UserName = "user@outlook.com",  // replace with valid value
+                        //    Password = "password"  // replace with valid value
+                        //};
+                        // This credentials is set in web.config file...
+
+                        //smtp.Credentials = credential;
+                        //smtp.Host = "smtp.emailsrvr.com";
+                        //smtp.Port = 587;
+                        //smtp.EnableSsl = false;
+
+                        await smtp.SendMailAsync(message);
+                        return RedirectToAction("Sent");
+                    }
+                }
+                return View(model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
---
+++ EmailSendExampleApp/Models/EmailSendModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmailSendExampleApp.Models
+{
+    public class EmailSendModel
+    {
+        [Required]
+        [Display(Name ="Your Name")]
+        public string SenderName { get; set; }
+        [Required]
+        [Display(Name = "Your Email Address")]
+        public string SenderEmail { get; set; }
+        [Display(Name = "Your Message")]
+        public string Message { get; set; }
+
+    }
+}
